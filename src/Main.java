493ef44208a1e4f3f345bb21069b626b6d1900c9
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-09-06 15:44:02 +0200

    classes created,getters and setters

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Circle circle = new Circle();
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println(rectangle);
+    }
+}
